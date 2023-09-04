ccfc39fabfd1f4181c0dbb1b17f8d08dee2148c7
Chris Daly <cdaly3.exe@gmail.com>
2023-09-03 22:32:16 -0400

    Making progress towards API calls

--- main.go
+++ main.go
@@ -2,16 +2,46 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/chrisdaly3/chiclets/database"
 	"github.com/chrisdaly3/chiclets/tui"
+	"github.com/chrisdaly3/chiclets/tui/constants"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
 	database.ConnectDB()
+	allTeams := getTeams()
+
 	p := tea.NewProgram(&tui.InitModel, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error initializing tui: %v", err)
 	}
+	fmt.Println(allTeams)
+}
+
+func getTeams() gjson.Result {
+
+	response, err := http.Get(constants.TeamsURL)
+	if err != nil {
+		fmt.Printf("Error communicating with NHL API: %v", err)
+	}
+	defer response.Body.Close()
+	if response.StatusCode > 299 {
+		slog.Error("Unhealthy Response", "response", response.Body)
+	}
+
+	responseBody, err := io.ReadAll(response.Body)
+	if err != nil {
+		slog.Error("Failure reading json response body")
+		os.Exit(1)
+	}
+
+	teams := gjson.Get(string(responseBody), "teams.#.name")
+	return teams
 }
---
+++ tui/constants/constants.go
@@ -0,0 +1,6 @@
+package constants
+
+var (
+	BaseURL  = "https://statsapi.web.nhl.com/api/v1"
+	TeamsURL = "https://statsapi.web.nhl.com/api/v1/teams"
+)
