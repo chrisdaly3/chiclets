bb90e60cf16929e21dfb025118bce31a5ffaaa1a
Chris Daly <cdaly@urbn.com>
2023-09-05 18:22:58 -0400

    trivial: remove println in main

--- main.go
+++ main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/chrisdaly3/chiclets/data"
 	"github.com/chrisdaly3/chiclets/tui"
 )
 
 func main() {
 	tui.InitModel.PopulateTable()
 
-	fmt.Println(data.TeamsTable)
 	p := tea.NewProgram(&tui.InitModel, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error initializing tui: %v", err)
