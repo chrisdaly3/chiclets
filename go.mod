279298d16f4bb0c37cf5d8b63181dffe58f3ad18
Chris Daly <cdaly@urbn.com>
2023-09-05 18:18:54 -0400

    Big Refactor: why did I try to use a db?

--- go.mod
+++ go.mod
@@ -6,8 +6,6 @@ require (
 	github.com/76creates/stickers v1.3.1-0.20230410064447-c0cf398aec57
 	github.com/charmbracelet/bubbletea v0.24.1
 	github.com/tidwall/gjson v1.16.0
-	gorm.io/driver/sqlite v1.5.3
-	gorm.io/gorm v1.25.4
 )
 
 require (
@@ -24,12 +22,9 @@ require (
 	github.com/charmbracelet/bubbles v0.16.1
 	github.com/charmbracelet/lipgloss v0.7.1
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
--- main.go
+++ main.go
@@ -2,67 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log/slog"
-	"net/http"
-	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/chrisdaly3/chiclets/database"
+	"github.com/chrisdaly3/chiclets/data"
 	"github.com/chrisdaly3/chiclets/tui"
-	"github.com/chrisdaly3/chiclets/tui/constants"
-	"github.com/tidwall/gjson"
 )
 
 func main() {
-	allTeams := getTeams()
-	database.ConnectDB()
+	tui.InitModel.PopulateTable()
 
+	fmt.Println(data.TeamsTable)
 	p := tea.NewProgram(&tui.InitModel, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error initializing tui: %v", err)
 	}
 
-	// TODO: this is just in place for debug.
-	// I will remove it as I progress and can validate data
-	// in the db itself.
-	Flyers := allTeams[3].Get("@values")
-	fmt.Println(Flyers)
-	//CreateTeamDB(allTeams)
 }
-
-func getTeams() []gjson.Result {
-	var teams []gjson.Result
-
-	response, err := http.Get(constants.TeamsURL)
-	if err != nil {
-		fmt.Printf("Error communicating with NHL API: %v", err)
-	}
-	defer response.Body.Close()
-	if response.StatusCode > 299 {
-		slog.Error("Unhealthy Response", "response", response.Body)
-	}
-
-	responseBody, err := io.ReadAll(response.Body)
-	if err != nil {
-		slog.Error("Failure reading json response body")
-		os.Exit(1)
-	}
-
-	// Check response for number of teams
-	responseTeamCount := gjson.GetBytes(responseBody, "teams.#")
-
-	// Use result of team number to iterate all team details
-	for i := 0; int64(i) < responseTeamCount.Int(); i++ {
-		teamIndex := fmt.Sprintf("teams.%v", i)
-		gjFields := fmt.Sprintf("{%s.teamName,%s.locationName,%s.division.name}.@join", teamIndex, teamIndex, teamIndex)
-
-		team := gjson.GetBytes(responseBody, gjFields)
-		teams = append(teams, team)
-	}
-	return teams
-}
-
-/*func CreateTeamDB(results []gjson.Result) error {
-
-}*/
