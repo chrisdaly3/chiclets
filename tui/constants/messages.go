56aa8273c0354e6f3eb22b6b9cff79139b1701a1
Chris Daly <cdaly3.exe@gmail.com>
2023-09-13 18:59:44 -0400

    feature: add keybindings for table navigation

---
+++ tui/constants/messages.go
@@ -0,0 +1,3 @@
+package constants
+
+type SelectionMessage struct{}
--- tui/tui.go
+++ tui/tui.go
@@ -1,12 +1,15 @@
 package tui
 
 import (
+	"fmt"
+
 	"github.com/76creates/stickers/flexbox"
 	"github.com/76creates/stickers/table"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/chrisdaly3/chiclets/data"
+	"github.com/chrisdaly3/chiclets/tui/constants"
 )
 
 // view keeps track
@@ -38,6 +41,10 @@ func (ui *UIModel) PopulateTable() {
 	ui.table.AddRows(data.TeamsTable)
 }
 
+func (ui *UIModel) selected() tea.Msg {
+	return constants.SelectionMessage{}
+}
+
 func (ui *UIModel) Init() tea.Cmd { return nil }
 
 func (ui *UIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -47,12 +54,34 @@ func (ui *UIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		ui.table.SetWidth(msg.Width)
 		ui.table.SetHeight(msg.Height - ui.flex.GetHeight())
 
+		// TODO: Either find a way to access values on position,
+		// OR uncover a better way to handle the selected item for stats
+		// on the teamNav view after selection. Might require some adjustments
+		// to the initial API response
+	case constants.SelectionMessage:
+		column, row := ui.table.GetCursorLocation()
+		fmt.Print(column, row)
 	// Add All Keybindings here
 	case tea.KeyMsg:
 		// might need to add conditional logic for view / state consideration here
 		switch {
 		case key.Matches(msg, Keybindings.Quit):
 			return ui, tea.Quit
+
+		case key.Matches(msg, Keybindings.Down):
+			ui.table.CursorDown()
+
+		case key.Matches(msg, Keybindings.Up):
+			ui.table.CursorUp()
+
+		case key.Matches(msg, Keybindings.Left):
+			ui.table.CursorLeft()
+
+		case key.Matches(msg, Keybindings.Right):
+			ui.table.CursorRight()
+
+		case key.Matches(msg, Keybindings.Select):
+			return ui, ui.selected
 		}
 	}
 	return ui, nil
